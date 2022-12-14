6f52d1dfd16f97d26794b49d8452ed40c7d94ade
RaimisReej <54449879+RaimisReej@users.noreply.github.com>
2022-12-14 14:04:01 +0000

    ignite

---
+++ src/api.js
@@ -0,0 +1,9 @@
+//My API_KEY
+const API_KEY = `?key=e2b883d7d3514669ba40b2b1b55f9220`;
+
+//Base URL
+const base_url = 'https://api.rawg.io/api';
+
+// popular games
+
+const popular_games = 'https://api.rawg.io/api/games'
